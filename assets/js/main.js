3cec31c01cc71bf2ae23a2f144de2d0b0080e85c
Asif Seferov <asif.seferov.95@bk.ru>
2020-07-21 23:18:20 +0400

    add third section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,6 @@
-        var $slider = $('#banner-area');
-        var activeClass = 'active-one';
-        $slider.bxSlider({
-            auto: true,
-            pager: false,
-            onSliderLoad: function() {
-                $slider.children('li').eq(1).addClass(activeClass);
-            },
-            onSlideAfter: function($slideElement, oldIndex, newIndex) {
-                $slider.children('li').removeClass(activeClass).eq(newIndex + 1).addClass(activeClass);
-        }
-    });
+var slSlider = $('#slider-area').bxSlider({
+    auto: true,
+    onSlideAfter: function(){
+      slSlider.startAuto();
+    }
+  });
