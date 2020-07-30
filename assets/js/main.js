85b5f6472254e265482a30f9400868f0dd94574d
Asif Seferov <asif.seferov.95@bk.ru>
2020-07-30 12:17:09 +0400

    add seven section

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,20 @@ $('.owl-carousel').owlCarousel({
       }
   }
 })
+$('#second-carousel').owlCarousel({
+  autoplay:true,
+  loop:true,
+  nav:true,
+  responsive:{
+      0:{
+        
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
